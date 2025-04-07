e1cea8b4cb3b93a3bdf03c31d340d92f7520a007
Rwema707 <chrisrwema90@gmail.com>
2025-04-07 16:11:19 -0700

    Network1

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" A script that: accepts a URL as input,
-sends a request to that URL, and displays the body of the response.
-"""
+"""Script that takes in a URL, sends a request to
+the URL and displays the body of the response"""
 import sys
 import requests
+
 
 if __name__ == "__main__":
     url = sys.argv[1]
